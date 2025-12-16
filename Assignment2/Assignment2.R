60cdc53b50f5781f27b9f949444784ee16920f6a
nocheatoriginal <78090539+nocheatoriginal@users.noreply.github.com>
2025-12-16 16:16:41 +0800

    Loading data set into R

--- Assignment2/Assignment2.R
+++ Assignment2/Assignment2.R
@@ -1,1 +1,9 @@
-print("Hello World!")
+setwd("~/Desktop/HHN/Sem-6/courses/CPC351/CPC351-repo/Assignment2")
+
+food_nutrition_df <- read.csv("Data/Food_Nutrition_Dataset.csv")
+
+summary(food_nutrition_df)
+
+
+food_nutrition_values <- food_nutrition_df[,3:8]
+summary(food_nutrition_values)
